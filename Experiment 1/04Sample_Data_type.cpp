e428648e7ff55ab826fe0c972a3b5a3fc030c213
Razeac <42296793+Razeac@users.noreply.github.com>
2022-09-18 22:02:49 +0800

    Add files via upload

---
+++ Experiment 1/04Sample_Data_type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char Age = 20;
+    int Width = 150, Height = 430, Value = 79;
+    float Money = 9.5;
+    double pi = 3.1415926535;
+    cout << "he is " << Age << " years old" << endl;
+    cout << "The rectangle is " << Width << " by " << Height << " units. \n";
+    cout << Value << " in decimal" << endl;
+    cout << Money << " data type should be a float" << endl;
+    return 0;
+}
