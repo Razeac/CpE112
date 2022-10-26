76d724eafa0aa44786b2fc9b16811a256fe27746
Razeac <42296793+Razeac@users.noreply.github.com>
2022-10-26 23:06:55 +0800

    Add files via upload

---
+++ Experiment_12/02Sample02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void myFunction(string fname) {
+cout << fname << " Refsnes\n";}
+int main(){
+    myFunction("Liam");
+    myFunction("Jenny");
+    myFunction("Anja");
+return 0;}
